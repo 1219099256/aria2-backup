50539e4ca300479447114544632f72d4cd7c8177
1219099256 <63403797+1219099256@users.noreply.github.com>
2021-02-04 13:53:01 +0800

    update

--- rcloneupload.sh
+++ rcloneupload.sh
@@ -8,10 +8,9 @@ if [ -e "$LocalDIR"*.rar ];then
   mv "$LocalDIR"*.rar "$LocalDIR"GIRLS.rar;
   unrar x -p'mrcong.com' "$LocalDIR"GIRLS.rar "$LocalDIR"pictures/;
   rm -rf "$LocalDIR"GIRLS.rar;
-  LocalDIR="/root/downloads/pictures/";
 fi
 
-rclone move "/content/downloads${File/#$LocalDIR}" "/content/drives/od/" --config /usr/local/sessionSettings/rclone.conf --fast-list --user-agent "Mozilla" --transfers 20 --checkers 20 --drive-server-side-across-configs -c --buffer-size 256M --drive-chunk-size 256M --drive-upload-cutoff 256M --drive-acknowledge-abuse --drive-keep-revision-forever --tpslimit 95 --tpslimit-burst 40 --stats-one-line --bwlimit 10M --stats=5s -v
+rclone move "/content/downloads/" "/content/drives/od/" --config /usr/local/sessionSettings/rclone.conf --fast-list --user-agent "Mozilla" --transfers 20 --checkers 20 --drive-server-side-across-configs -c --buffer-size 256M --drive-chunk-size 256M --drive-upload-cutoff 256M --drive-acknowledge-abuse --drive-keep-revision-forever --tpslimit 95 --tpslimit-burst 40 --stats-one-line --bwlimit 10M --stats=5s -v
 
 cat /content/upload.txt << EOF
 "/content/downloads${File/#$LocalDIR/}"
