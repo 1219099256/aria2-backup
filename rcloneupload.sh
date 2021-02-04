5cf54bbecf696c035263e99bebef4f7d93dd56a6
1219099256 <63403797+1219099256@users.noreply.github.com>
2021-02-04 10:49:58 +0800

    Update rcloneupload.sh

--- rcloneupload.sh
+++ rcloneupload.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-if [ -e downloads/*.rar ];then
-  mkdir /root/downloads/pictures;
-  mv downloads/*.rar downloads/GIRLS.rar;
-  unrar x -p'mrcong.com' downloads/GIRLS.rar downloads/pictures/;
-  rm -rf downloads/GIRLS.rar;
+File="$3";
+LocalDIR="/content/downloads/"
+
+if [ -e "$LocalDIR"*.rar ];then
+  mkdir "$LocalDIR"pictures;
+  mv "$LocalDIR"*.rar "$LocalDIR"GIRLS.rar;
+  unrar x -p'mrcong.com' "$LocalDIR"GIRLS.rar "$LocalDIR"pictures/;
+  rm -rf "$LocalDIR"GIRLS.rar;
   LocalDIR="/root/downloads/pictures/";
 fi
 
-rclone move "$3" "/content/drives/od/" --config /usr/local/sessionSettings/rclone.conf --fast-list --user-agent "Mozilla" --transfers 20 --checkers 20 --drive-server-side-across-configs -c --buffer-size 256M --drive-chunk-size 256M --drive-upload-cutoff 256M --drive-acknowledge-abuse --drive-keep-revision-forever --tpslimit 95 --tpslimit-burst 40 --stats-one-line --bwlimit 10M --stats=5s -v
+rclone move "/content/downloads${File/#$LocalDIR}" "/content/drives/od/" --config /usr/local/sessionSettings/rclone.conf --fast-list --user-agent "Mozilla" --transfers 20 --checkers 20 --drive-server-side-across-configs -c --buffer-size 256M --drive-chunk-size 256M --drive-upload-cutoff 256M --drive-acknowledge-abuse --drive-keep-revision-forever --tpslimit 95 --tpslimit-burst 40 --stats-one-line --bwlimit 10M --stats=5s -v
+
+cat /content/upload.txt << EOF
+"/content/downloads${File/#$LocalDIR/}"
+EOF
