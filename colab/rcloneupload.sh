4afc52db71347f7ba1dad405b81a95385aff2c0f
1219099256 <63403797+1219099256@users.noreply.github.com>
2021-02-05 18:55:46 +0800

    update

--- colab/rcloneupload.sh
+++ colab/rcloneupload.sh
@@ -14,5 +14,5 @@ fi
 rclone move "${LocalDIR}" "${RemoteDIR}" --config /usr/local/sessionSettings/rclone.conf --fast-list --user-agent "Mozilla" --transfers 20 --checkers 20 --drive-server-side-across-configs -c --buffer-size 256M --drive-chunk-size 256M --drive-upload-cutoff 256M --drive-acknowledge-abuse --drive-keep-revision-forever --tpslimit 95 --tpslimit-burst 40 --stats-one-line --bwlimit 10M --stats=5s -v
 
 cat >> /content/upload.txt << EOF
-"$3"
+$3
 EOF
