617b7bd8279e9932501cdff82283d9482b7234f4
1219099256 <63403797+1219099256@users.noreply.github.com>
2021-02-05 16:01:58 +0800

    update

---
+++ colab/aria2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget -O aria2.tar.bz2 https://github.com/q3aql/aria2-static-builds/releases/download/v1.35.0/aria2-1.35.0-linux-gnu-64bit-build1.tar.bz2
+tar -jxvf aria2.tar.bz2
+cd aria2 && make install
--- rcloneupload.sh
+++ rcloneupload.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 File="$3";
-LocalDIR="/content/downloads/";
-RemoteDIR="/content/drive/od/";
+LocalDIR="/root/downloads/";
+RemoteDIR="/root/od/";
 
 if [ -e $LocalDIR*.rar ];then
-  echo mrcong.com | unrar x ${File} ${LocalDIR}pictures/;
   LocalDIR="${LocalDIR}pictures/";
   RemoteDIR="${RemoteDIR}pictures/";
-  rm -rf File;
+  unrar -p"mrcong.com" x ${File} ${LocalDIR};
+  rm -rf ${File};
 fi
 
-rclone move "${LocalDIR}" "${RemoteDIR}" --config /usr/local/sessionSettings/rclone.conf --fast-list --user-agent "Mozilla" --transfers 20 --checkers 20 --drive-server-side-across-configs -c --buffer-size 256M --drive-chunk-size 256M --drive-upload-cutoff 256M --drive-acknowledge-abuse --drive-keep-revision-forever --tpslimit 95 --tpslimit-burst 40 --stats-one-line --bwlimit 10M --stats=5s -v
+rclone move "${LocalDIR}" "${RemoteDIR}"  --config rclone.conf;
 
 cat >> /root/upload.txt << EOF
-"$3"
+$3
 EOF
