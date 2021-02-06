0ec8b3ae5ccc929ba425e113af947ea527356742
1219099256 <63403797+1219099256@users.noreply.github.com>
2021-02-06 18:20:29 +0800

    Update rcloneupload.sh

--- colab/rcloneupload.sh
+++ colab/rcloneupload.sh
@@ -5,13 +5,13 @@ LocalDIR="/content/downloads/";
 RemoteDIR="/content/drives/od/";
 
 if [ -e $LocalDIR*.rar ];then
-  LocalDIR="${LocalDIR}pictures/";
-  RemoteDIR="${RemoteDIR}pictures/";
   unrar -p"mrcong.com" x ${File} ${LocalDIR};
   rm -rf ${File};
+  File="${LocalDIR}pictures/";
+  RemoteDIR="${RemoteDIR}pictures/";
 fi
 
-rclone move "${LocalDIR}" "${RemoteDIR}" --config /usr/local/sessionSettings/rclone.conf --fast-list --user-agent "Mozilla" --transfers 20 --checkers 20 --drive-server-side-across-configs -c --buffer-size 256M --drive-chunk-size 256M --drive-upload-cutoff 256M --drive-acknowledge-abuse --drive-keep-revision-forever --tpslimit 95 --tpslimit-burst 40 --stats-one-line --bwlimit 10M --stats=5s -v
+rclone move "${File}" "${RemoteDIR}" --config /usr/local/sessionSettings/rclone.conf --fast-list --user-agent "Mozilla" --transfers 20 --checkers 20 --drive-server-side-across-configs -c --buffer-size 256M --drive-chunk-size 256M --drive-upload-cutoff 256M --drive-acknowledge-abuse --drive-keep-revision-forever --tpslimit 95 --tpslimit-burst 40 --stats-one-line --bwlimit 10M --stats=5s -v
 
 cat >> /content/upload.txt << EOF
 $3
