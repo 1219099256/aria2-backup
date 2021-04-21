6549f5c5474378f00ad6f50a06dafea0f4f4ef96
1219099256 <63403797+1219099256@users.noreply.github.com>
2021-04-21 18:29:02 +0800

    Update rcloneupload.sh

--- rclone/rcloneupload.sh
+++ rclone/rcloneupload.sh
@@ -4,15 +4,15 @@ File="$3";
 LocalDIR="/root/downloads/";
 RemoteDIR="/root/od/";
 
-if [ -e $LocalDIR*.rar ];then
-  unrar -p"mrcong.com" x ${File} ${LocalDIR}pictures/;
-  rm -rf ${File};
+if [ -n "`find ${LocalDIR} -name '*.rar'`" ];then
+  unrar -p"mrcong.com" x "${File}" "${LocalDIR}pictures/";
+  rm -rf "${File}";
   File="${LocalDIR}pictures/";
   RemoteDIR="${RemoteDIR}pictures/";
+  else
+  curl -G https://sre24.com/api/v1/push \
+        -d token=1db8e4d7d5d42399cc72f26a7eac5eb8 \
+        --data-urlencode msg="$3"
 fi
 
 rclone move "${File}" "${RemoteDIR}";
-
-cat >> /root/upload.txt << EOF
-$3
-EOF
